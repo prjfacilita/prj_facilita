bbedad05b160681c2513b60f03b893f49bbf1bca
Rodrigo Teles <rtelesc@gmail.com>
2018-11-26 17:43:21 -0200

    [DEV] Atualização API;

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -1,20 +1,20 @@
 <?php
-//namespace App\Http\Controllers\API;
-//use Illuminate\Http\Request;
-//use App\Http\Controllers\Controller;
-//use App\Login;
-//use Illuminate\Support\Facades\Auth;
-//
-//use \Validator;
-//class UserController extends Controller
-//{
+namespace App\Http\Controllers\API;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Login;
+use Illuminate\Support\Facades\Auth;
+
+use \Validator;
+class UserController extends Controller
+{
 //    public $successStatus = 200;
-//    /**
-//     * login api
-//     *
-//     * @return \Illuminate\Http\Response
-//     */
-//    public function login(){
+    /**
+     * login api
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function login(){
 //        if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){
 //            $user = Auth::user();
 //            return response()->json(['success'], $this-> successStatus);
@@ -22,14 +22,14 @@
 //        else{
 //            return response()->json(['error'=>'Unauthorised'], 401);
 //        }
-//    }
-//    /**
-//     * Register api
-//     *
-//     * @return \Illuminate\Http\Response
-//     */
-//    public function register(Request $request)
-//    {
+    }
+    /**
+     * Register api
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function register(Request $request)
+    {
 //        $validator = Validator::make($request->all(), [
 //            'email' => 'required|email',
 //            'password' => 'required',
@@ -45,15 +45,15 @@
 ////        $success['token'] =  $user->createToken('MyApp')-> accessToken;
 ////        $success['name'] =  $user->name;
 //        return response()->json(['success'], $this-> successStatus);
-//    }
-//    /**
-//     * details api
-//     *
-//     * @return \Illuminate\Http\Response
-//     */
-//    public function details()
-//    {
+    }
+    /**
+     * details api
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function details()
+    {
 //        $user = Auth::user();
 //        return response()->json(['success' => $user], $this-> successStatus);
-//    }
-//}
+    }
+}
