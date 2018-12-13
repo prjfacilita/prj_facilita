f8a749f945c34d661bab57cc2f7cf374b226eefa
Rodrigo Teles <rtelesc@gmail.com>
2018-12-13 11:34:37 -0200

    teste

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,8 +23,8 @@
 
 
     <script>
-        window.setTimeout( function(){
-            window.location = "/prj_facilita/public/pedido";
-        }, 300 );
+        // window.setTimeout( function(){
+        //     window.location = "/prj_facilita/public/pedido";
+        // }, 300 );
     </script>
 @endsection
