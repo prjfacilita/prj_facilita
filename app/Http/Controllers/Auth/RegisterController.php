54749a426f321241f81f710d181ac774a52d3f29
Rodrigo Teles <rtelesc@gmail.com>
2018-12-13 00:43:10 -0200

    teste

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -25,7 +25,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/public/';
 
     /**
      * Create a new controller instance.
@@ -117,7 +117,7 @@ class RegisterController extends Controller
 
 
 
-       
+
 
         return new $user;
 
