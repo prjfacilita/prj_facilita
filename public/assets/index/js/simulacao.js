5ef691a0791d3391539c00fb0cf4be8df8502602
rtelesc <rtelesc@gmail.com>
2018-12-15 04:23:08 -0200

    atualização index

--- public/assets/index/js/simulacao.js
+++ public/assets/index/js/simulacao.js
@@ -5,7 +5,7 @@
 
 /*Click para simulação*/
 
-        let clicks = 0;
+        // let clicks = 0;
         let value = 0;
         let qtdParcelas = 0;
 
@@ -20,11 +20,6 @@
 
                 return false;
             }
-
-            clicks += 1;
-
-
-            if(clicks == 1) {
 
 
                 if (document.querySelector('input[name="simulation-value"]:checked').value) {
@@ -42,34 +37,36 @@
                 $('.banner__simulation').addClass('simulation-value-selected');
                 $('.banner__simulation').removeClass('simulation-value');
 
+
+
+        });
+
+
+
+        $(document).on('click','.simulation-plots', function(e) {
+
+            e.preventDefault();
+
+            if ( ! $("input[name=\"simulation-plots\"]:checked").is(':checked') ){
+
+                return false;
             }
 
-            if(clicks == 2){
-
-                if ( ! $("input[name=\"simulation-plots\"]:checked").is(':checked') ){
-
-                    return false;
-                }
-
-                $(".simulation-box form.pt2").css('display','none'); // ocultar formulario
-                $(".simulation-box h2.pt2").css('display','none'); // ocultar h2
-                $(".simulation-box p.pt2").css('display','none');
+            $(".simulation-box form.pt2").css('display','none'); // ocultar formulario
+            $(".simulation-box h2.pt2").css('display','none'); // ocultar h2
+            $(".simulation-box p.pt2").css('display','none');
 
 
-                $(".simulation-box form.pt3").css('display','block'); // ocultar formulario
-                $(".simulation-box p.pt3").css('display','block'); // ocultar h2
-                $(".simulation-box h2.pt3").css('display','block'); // ocultar h2
+            $(".simulation-box form.pt3").css('display','block'); // ocultar formulario
+            $(".simulation-box p.pt3").css('display','block'); // ocultar h2
+            $(".simulation-box h2.pt3").css('display','block'); // ocultar h2
 
-                $(".simulation-box p.pt3").html('R$' + formatReal( value ));
-
-
-                qtdParcelas = document.querySelector('#pt2 input[name="simulation-plots"]:checked').value;
-
-                $("input[name='simulation-plots2'][value='"+qtdParcelas+"']").prop('checked', true);
-
-            }
+            $(".simulation-box p.pt3").html('R$' + formatReal( value ));
 
 
+            qtdParcelas = document.querySelector('#pt2 input[name="simulation-plots"]:checked').value;
+
+            $("input[name='simulation-plots2'][value='"+qtdParcelas+"']").prop('checked', true);
 
         });
 
