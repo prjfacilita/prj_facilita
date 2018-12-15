65788219545299cb5ae664a39c38db51395628fd
rtelesc <rtelesc@gmail.com>
2018-12-15 04:18:11 -0200

    atualização index

--- public/assets/index/js/simulacao.js
+++ public/assets/index/js/simulacao.js
@@ -24,8 +24,29 @@
             clicks += 1;
 
 
+            $(".simulation-box form.pt1").css('display','none'); // ocultar formulario 1
+            $(".simulation-box h2.pt1").css('display','none'); // ocultar h2
+            $(".simulation-box form.pt2").css('display','block'); // ocultar formulario
+            $(".simulation-box p.pt2").css('display','block'); // ocultar h2
+            $(".simulation-box h2.pt2").css('display','block'); // ocultar h2
+            $(".simulation-box p.pt2").html('R$ ' + formatReal( value ));
+            $('.banner__simulation').addClass('simulation-value-selected');
+            $('.banner__simulation').removeClass('simulation-value');
+
+
 
         });
+
+
+        /*FUNÇÃO PAARA FORMATAR O VALOR*/
+        function formatReal( int ) {
+            var tmp = int+'';
+            tmp = tmp.replace(/([0-9]{2})$/g, ",$1");
+            if( tmp.length > 6 )
+                tmp = tmp.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
+            return tmp;
+        }
+
 
 
 
