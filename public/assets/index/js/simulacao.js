87972f30064e2f266ba42724ce2f62800bc1e7e4
rtelesc <rtelesc@gmail.com>
2018-12-17 12:13:54 -0200

    atualização formulario

--- public/assets/index/js/simulacao.js
+++ public/assets/index/js/simulacao.js
@@ -105,5 +105,19 @@
 
 
 
+            $(document).ready(function(){
+                $("#money").inputmask('decimal', {
+                    'alias': 'numeric',
+                    'groupSeparator': ',',
+                    'autoGroup': true,
+                    'digits': 2,
+                    'radixPoint': ".",
+                    'digitsOptional': false,
+                    'allowMinus': false,
+                    'prefix': 'R$ ',
+                    'placeholder': ''
+                })});
+
+
 
 
