70370ed6c86eea4650e2992a26d2247a3961e196
rtelesc <rtelesc@gmail.com>
2018-12-19 10:08:24 -0200

    atualização mirgations

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -168,8 +168,14 @@
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<script src="{{asset('node_modules/jquery-mask-plugin/dist/jquery.mask.js')}}"></script>
 
-<script src="node_modules/jQuery-Mask-Plugin-master/dist/jquery.mask.js"></script>
+
+<script src="{{asset('node_modules/inputmask/dist/inputmask/inputmask.js')}}"></script>
+<script src="{{asset('node_modules/inputmask/dist/inputmask/inputmask.extensions.js')}}"></script>
+<script src="{{asset('node_modules/inputmask/dist/inputmask/inputmask.numeric.extensions.js')}}"></script>
+<script src="{{asset('node_modules/inputmask/dist/inputmask/inputmask.date.extensions.js')}}"></script>
+<script src="{{asset('node_modules/inputmask/dist/inputmask/jquery.inputmask.js')}}"></script>
 
 <script>
     $('#cpf').mask('000.000.000-00', {reverse: true});
