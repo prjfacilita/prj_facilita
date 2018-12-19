cc6a17030ba1b34705f1f6bd905b08122e4cf8fd
rtelesc <rtelesc@gmail.com>
2018-12-18 23:29:51 -0200

    atualização engine email

--- resources/views/emprestimo/pedido.blade.php
+++ resources/views/emprestimo/pedido.blade.php
@@ -43,9 +43,19 @@
             </div>
 
 
-            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                {{ csrf_field() }}
-            </form>
+            <ul class="dropdown-menu" role="menu">
+                <li>
+                    <a href="{{ route('logout') }}"
+                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                        Logout
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        {{ csrf_field() }}
+                    </form>
+                </li>
+            </ul>
         </div>
     </div>
 </header>
