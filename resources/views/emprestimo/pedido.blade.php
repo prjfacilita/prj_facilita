f94fe1e81d05d330ab957a70df29fe49cbd3cd9d
rtelesc <rtelesc@gmail.com>
2018-12-18 23:31:32 -0200

    atualização

--- resources/views/emprestimo/pedido.blade.php
+++ resources/views/emprestimo/pedido.blade.php
@@ -43,8 +43,7 @@
             </div>
 
 
-            <ul class="dropdown-menu" role="menu">
-                <li>
+
                     <a href="{{ route('logout') }}"
                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
@@ -54,8 +53,7 @@
                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                         {{ csrf_field() }}
                     </form>
-                </li>
-            </ul>
+
         </div>
     </div>
 </header>
