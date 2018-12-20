e810871605e0e08ba408bbf599fa65730546cf87
rtelesc <rtelesc@gmail.com>
2018-12-20 11:20:27 -0200

    atualização simulacao

--- database/migrations/2018_12_19_183955_create_simulacao_table.php
+++ database/migrations/2018_12_19_183955_create_simulacao_table.php
@@ -21,6 +21,7 @@ class CreateSimulacaoTable extends Migration
             $table->string('dataPrimeiraParcela');
             $table->string('tarifaCadastro');
             $table->string('ValorParcela');
+            $table->integer('user_id')->default(0);
             $table->timestamps();
         });
     }
