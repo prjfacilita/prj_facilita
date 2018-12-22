3c5bb207268db3626bf623c3cb1543faf4b0ffba
rtelesc <rtelesc@gmail.com>
2018-12-21 23:01:09 -0200

    atualizacao

--- database/migrations/2018_12_21_183430_cadastro.php
+++ database/migrations/2018_12_21_183430_cadastro.php
@@ -43,33 +43,33 @@ TIPO_RES
 TEL_FIXO
 TEL_CEL*/
             $table->increments('id');
-            $table->text('nome_completo')->default(0); // nome completo aceita apenas 30 caracteres, validado no back-end e no front-end
-            $table->text('email')->default(0);
-            $table->text('dt_nasc')->default(0);
-            $table->text('nat_ocup')->default(0);
-            $table->text('sexo')->default(0); // sexo
-            $table->text('nacionalidade')->default(0); // nacionalidade
-            $table->text('cidade')->default(0); // cidade de nascimento
-            $table->text('uf_nasc')->default(0); // unidade federal de nascimento
-            $table->text('tp_doc')->default(0); // tipo do documento
-            $table->text('nr_doc')->default(0); // número do documento
-            $table->text('emissor'); // orgão emissor do documento
-            $table->text('nome_mae')->default(0); // nome completo da mãe
-            $table->text('grau_instr')->default(0);
-            $table->text('nome_conj')->default(0);
-            $table->text('cpf_conj')->default(0);
-            $table->text('dt_nasc_conj')->default(0);
-            $table->text('sexo_conj')->default(0);
-            $table->text('cep_res')->default(0);
-            $table->text('end_res')->default(0);
-            $table->text('num_res')->default(0);
-            $table->text('compl_res')->default(0);
-            $table->text('bairro_res')->default(0);
-            $table->text('cidade_res')->default(0);
-            $table->text('uf_res')->default(0);
-            $table->text('tipo_res')->default(0);
-            $table->text('tel_fixo')->default(0);
-            $table->text('tel_cel')->default(0);
+            $table->string('nome_completo')->default(0); // nome completo aceita apenas 30 caracteres, validado no back-end e no front-end
+            $table->string('email')->default(0);
+            $table->string('dt_nasc')->default(0);
+            $table->string('nat_ocup')->default(0);
+            $table->string('sexo')->default(0); // sexo
+            $table->string('nacionalidade')->default(0); // nacionalidade
+            $table->string('cidade')->default(0); // cidade de nascimento
+            $table->string('uf_nasc')->default(0); // unidade federal de nascimento
+            $table->string('tp_doc')->default(0); // tipo do documento
+            $table->string('nr_doc')->default(0); // número do documento
+            $table->string('emissor'); // orgão emissor do documento
+            $table->string('nome_mae')->default(0); // nome completo da mãe
+            $table->string('grau_instr')->default(0);
+            $table->string('nome_conj')->default(0);
+            $table->string('cpf_conj')->default(0);
+            $table->string('dt_nasc_conj')->default(0);
+            $table->string('sexo_conj')->default(0);
+            $table->string('cep_res')->default(0);
+            $table->string('end_res')->default(0);
+            $table->string('num_res')->default(0);
+            $table->string('compl_res')->default(0);
+            $table->string('bairro_res')->default(0);
+            $table->string('cidade_res')->default(0);
+            $table->string('uf_res')->default(0);
+            $table->string('tipo_res')->default(0);
+            $table->string('tel_fixo')->default(0);
+            $table->string('tel_cel')->default(0);
             $table->integer('operador')->default(0);
             $table->timestamps();
         });
