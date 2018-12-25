43c5afd0569457940ce6f585d580eb003bf335da
rtelesc <rtelesc@gmail.com>
2018-12-25 20:06:00 -0200

    teste

--- app/Http/Controllers/EmprestimoController.php
+++ app/Http/Controllers/EmprestimoController.php
@@ -195,6 +195,7 @@ class EmprestimoController extends Controller
         $dados_pessoais->sexo_conj          =   $request->sexo_conjuge;
         $dados_pessoais->pb_exposta         =   $request->pb_exposta;
         $dados_pessoais->nome_mae           =   $request->nome_mae;
+        $dados_pessoais->emissor            =   $request->solicitation_organ;
 
 
 //        $dados_pessoais->emissor            =   '1';
