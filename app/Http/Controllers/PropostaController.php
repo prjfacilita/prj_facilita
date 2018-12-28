55d3ad2a3339e2a311503797e7400ee01bb61617
rtelesc <rtelesc@gmail.com>
2018-12-28 09:54:37 -0200

    atualização

--- app/Http/Controllers/PropostaController.php
+++ app/Http/Controllers/PropostaController.php
@@ -13,7 +13,6 @@ use GuzzleHttp\Client;
 use Illuminate\Support\Facades\DB;
 //use App\Http\Controllers\EmprestimoController
 use App\Http\Controllers\DadosBancariosController;
-use HttpRequest;
 class PropostaController extends Controller
 {
     //
@@ -126,67 +125,36 @@ class PropostaController extends Controller
 
 //        $token = session('token_key');
 
+        <?php
 
+$curl = curl_init();
 
-$request = new HttpRequest();
-$request->setUrl('https://c2gvw4lxh9.execute-api.sa-east-1.amazonaws.com/hmg/api/v2/ep/propostas/055090000030/analisecadastral');
-$request->setMethod(HTTP_METH_PUT);
-
-$request->setHeaders(array(
-    'Postman-Token' => '125eb395-03b5-403f-98ce-3a1f2fbd034d',
-    'cache-control' => 'no-cache',
-    'Authorization' => 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Im9uRXlwQllvVWY1QnFjYXFMOHRtWEZrQTRxcyJ9.eyJhdWQiOiJtaWNyb3NvZnQ6aWRlbnRpdHlzZXJ2ZXI6NzIxYzQwNzUtMmM2Zi00MWRkLWI2MDktM2Q3YzVhYWJiN2ZkIiwiaXNzIjoiaHR0cDovL2ZzLmNic3NkaWdpdGFsLmNvbS5ici9hZGZzL3NlcnZpY2VzL3RydXN0IiwiaWF0IjoxNTQ1OTkzMjM2LCJleHAiOjE1NDU5OTY4MzYsImNsaWVudF9hcHAiOiJGYWNpbGl0YSBFUCIsImNsaWVudF9jb21wYW55IjoiRmFjaWxpdGEiLCJhcHB0eXBlIjoiQ29uZmlkZW50aWFsIiwiYXBwaWQiOiI3MjFjNDA3NS0yYzZmLTQxZGQtYjYwOS0zZDdjNWFhYmI3ZmQiLCJhdXRobWV0aG9kIjoiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2F1dGhlbnRpY2F0aW9ubWV0aG9kL3Bhc3N3b3JkIiwiYXV0aF90aW1lIjoiMjAxOC0xMi0yOFQxMDozMzo1Ni4zMTlaIiwidmVyIjoiMS4wIn0.pQb7knwSbUnik8lgQAFeS1g47WQaiSjMlwqEo_2Ali1OXcxfmQqsHqdLdnk8xtJ9gNPApEmV4aURC38fWZoisgPyjSGPLIHQuy4_XSe-1EggAgnDRaLz76Pd_-2gaBLdgPejOToBB-iwYyyYCseOgqTYKDMGEnaIpKcOAdI4A1YTOxmVlo-F_EUOhm91RL_M4SJ-4Qfa4kbauFimwAFQL4Vdy3ST14iaLFvlgj-WaioLr0Xvxb7wUVHAm4ffXwJLIGbmemEUgU3qhO7XKUAjQ_oLAoUX_AT93-9cc7k332AqnXCTlOksQkqSIDf8c6W1L2dGj23jSKQzOe7EG6AQzA',
-    'Content-Type' => 'application/json'
+curl_setopt_array($curl, array(
+    CURLOPT_URL => "https://c2gvw4lxh9.execute-api.sa-east-1.amazonaws.com/hmg/api/v2/ep/propostas/055090000030/analisecadastral",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "PUT",
+    CURLOPT_POSTFIELDS => "{\n    \"nomeMae\": \"Maria da Silva\",\n    \"email\": \"email@email.com\",\n    \"estadoCivil\": \"SOLTEIRO\",\n    \"naturalidade\": \"São Paulo\",\n    \"valorPatrimonio\": \"5000\",\n    \"documentosPessoais\": [\n        {\n            \"numeroDocumento\": 125478991,\n            \"tipoDocumento\": \"RG\"\n        }\n    ],\n    \"endereco\": {\n        \"cep\": 11740000,\n        \"logradouro\": \"Rua Butantã\",\n        \"numero\": 123,\n        \"bairro\": \"Pinheiros\",\n        \"cidade\": \"Sao Paulo\",\n        \"complemento\": \"10o andar\"\n    },\n    \"enderecoComercial\": {\n        \"cep\": 11740000,\n        \"logradouro\": \"Rua Butantã\",\n        \"numero\": 123,\n        \"bairro\": \"Pinheiros\",\n        \"cidade\": \"Sao Paulo\",\n        \"uf\": \"SP\",\n        \"complemento\": \"10o andar\"\n    },\n    \"telefones\": [\n        {\n            \"ddd\": 11,\n            \"numero\": 985478547,\n            \"tipoTelefone\": \"CELULAR\",\n            \"ramal\": 444\n        }\n    ],\n    \"renda\": {\n        \"tipoComprovanteRenda\": \"EXTRATO_FGTS\"\n    }\n}",
+    CURLOPT_HTTPHEADER => array(
+        "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Im9uRXlwQllvVWY1QnFjYXFMOHRtWEZrQTRxcyJ9.eyJhdWQiOiJtaWNyb3NvZnQ6aWRlbnRpdHlzZXJ2ZXI6NzIxYzQwNzUtMmM2Zi00MWRkLWI2MDktM2Q3YzVhYWJiN2ZkIiwiaXNzIjoiaHR0cDovL2ZzLmNic3NkaWdpdGFsLmNvbS5ici9hZGZzL3NlcnZpY2VzL3RydXN0IiwiaWF0IjoxNTQ1OTkzMjM2LCJleHAiOjE1NDU5OTY4MzYsImNsaWVudF9hcHAiOiJGYWNpbGl0YSBFUCIsImNsaWVudF9jb21wYW55IjoiRmFjaWxpdGEiLCJhcHB0eXBlIjoiQ29uZmlkZW50aWFsIiwiYXBwaWQiOiI3MjFjNDA3NS0yYzZmLTQxZGQtYjYwOS0zZDdjNWFhYmI3ZmQiLCJhdXRobWV0aG9kIjoiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2F1dGhlbnRpY2F0aW9ubWV0aG9kL3Bhc3N3b3JkIiwiYXV0aF90aW1lIjoiMjAxOC0xMi0yOFQxMDozMzo1Ni4zMTlaIiwidmVyIjoiMS4wIn0.pQb7knwSbUnik8lgQAFeS1g47WQaiSjMlwqEo_2Ali1OXcxfmQqsHqdLdnk8xtJ9gNPApEmV4aURC38fWZoisgPyjSGPLIHQuy4_XSe-1EggAgnDRaLz76Pd_-2gaBLdgPejOToBB-iwYyyYCseOgqTYKDMGEnaIpKcOAdI4A1YTOxmVlo-F_EUOhm91RL_M4SJ-4Qfa4kbauFimwAFQL4Vdy3ST14iaLFvlgj-WaioLr0Xvxb7wUVHAm4ffXwJLIGbmemEUgU3qhO7XKUAjQ_oLAoUX_AT93-9cc7k332AqnXCTlOksQkqSIDf8c6W1L2dGj23jSKQzOe7EG6AQzA",
+        "Content-Type: application/json",
+        "Postman-Token: 91e90c32-e572-4e2c-a734-e698bdc1a2bd",
+        "cache-control: no-cache"
+    ),
 ));
 
-$request->setBody('{
-    "nomeMae": "Maria da Silva",
-    "email": "email@email.com",
-    "estadoCivil": "SOLTEIRO",
-    "naturalidade": "São Paulo",
-    "valorPatrimonio": "5000",
-    "documentosPessoais": [
-        {
-            "numeroDocumento": 125478991,
-            "tipoDocumento": "RG"
-        }
-    ],
-    "endereco": {
-        "cep": 11740000,
-        "logradouro": "Rua Butantã",
-        "numero": 123,
-        "bairro": "Pinheiros",
-        "cidade": "Sao Paulo",
-        "complemento": "10o andar"
-    },
-    "enderecoComercial": {
-        "cep": 11740000,
-        "logradouro": "Rua Butantã",
-        "numero": 123,
-        "bairro": "Pinheiros",
-        "cidade": "Sao Paulo",
-        "uf": "SP",
-        "complemento": "10o andar"
-    },
-    "telefones": [
-        {
-            "ddd": 11,
-            "numero": 985478547,
-            "tipoTelefone": "CELULAR",
-            "ramal": 444
-        }
-    ],
-    "renda": {
-        "tipoComprovanteRenda": "EXTRATO_FGTS"
-    }
-}');
+$response = curl_exec($curl);
+$err = curl_error($curl);
 
-try {
-    $response = $request->send();
+curl_close($curl);
 
-    echo $response->getBody();
-} catch (HttpException $ex) {
-    echo $ex;
+if ($err) {
+    echo "cURL Error #:" . $err;
+} else {
+    echo $response;
 }
         /*CRIAR VÁRIAVEL NO SISTEMA PARA DEFINIR O ACESSO DIRETO PARA A PÁGINA DE STATUS*/
 
