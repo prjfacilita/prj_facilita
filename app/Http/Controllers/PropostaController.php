a2e9fabd0e3d4cb036f5c007b9b8f89571dbf547
rtelesc <rtelesc@gmail.com>
2018-12-28 09:26:44 -0200

    atualização

--- app/Http/Controllers/PropostaController.php
+++ app/Http/Controllers/PropostaController.php
@@ -182,8 +182,8 @@ class PropostaController extends Controller
         $updateBody['nomeMae'] = "nome mae";
         $updateBody['naturalidade'] = "Santista teste";
         $updateBody["valorPatrimonio"] = "2000.0";
-        $updateBody["documentosPessoais"]["numeroDocumento"]=  125478991;
-        $updateBody["documentosPessoais"]["tipoDocumento"]=  "RG";
+        $updateBody["documentosPessoais"][]["numeroDocumento"]=  125478991;
+        $updateBody["documentosPessoais"][]["tipoDocumento"]=  "RG";
         $updateBody["endereco"]["cep"] = "117300000";
         $updateBody["endereco"]["logradouro"] = "117300000";
         $updateBody["endereco"]["numero"] = "815";
