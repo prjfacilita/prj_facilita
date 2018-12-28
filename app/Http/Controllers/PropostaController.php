7dc03e04b6d729adc3c31f77519170ce6af14b47
rtelesc <rtelesc@gmail.com>
2018-12-28 09:38:12 -0200

    atualização

--- app/Http/Controllers/PropostaController.php
+++ app/Http/Controllers/PropostaController.php
@@ -195,13 +195,15 @@ class PropostaController extends Controller
         $retorno01  =  $client->request('PUT', EmprestimoController::URL_ENDPOINT(). '/api/v2/ep/propostas/055090000030/analisecadastral',
             [
 
-                'json' => [
+
 
                 'body' => json_encode($updateBody, JSON_FORCE_OBJECT)
 
-                ]
+
             ]);
 
+
+        return json_encode($updateBody, JSON_FORCE_OBJECT);
 
         $arr = json_decode($retorno01->getBody());
         /*CRIAR VÁRIAVEL NO SISTEMA PARA DEFINIR O ACESSO DIRETO PARA A PÁGINA DE STATUS*/
