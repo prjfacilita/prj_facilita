d3bb9c373dbe0cbb43aadac2a02b52460608ba10
rtelesc <rtelesc@gmail.com>
2018-12-28 05:42:50 -0200

    teste

--- app/Http/Controllers/PropostaController.php
+++ app/Http/Controllers/PropostaController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\EmprestimoController;
 //use Illuminate\Http\Request;
 use GuzzleHttp\Client;
+use Illuminate\Support\Facades\DB;
 
 class PropostaController extends Controller
 {
