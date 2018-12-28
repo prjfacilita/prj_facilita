2893dd6995b1a61c8d4135e7da8361400b5209b2
rtelesc <rtelesc@gmail.com>
2018-12-28 10:08:09 -0200

    atualização

--- app/Http/Controllers/DadosBancariosController.php
+++ app/Http/Controllers/DadosBancariosController.php
@@ -28,9 +28,9 @@ class DadosBancariosController extends Controller
         $dados_bancarios->banco = $request->banco_id;
         $dados_bancarios->agencia = $request->nro_agencia;
         $dados_bancarios->dig_ag = substr($request->nro_agencia, -1);
-        $dados_bancarios->conta = '4508';
-        $dados_bancarios->tipo = 'corrente conta';
-        $dados_bancarios->conta_desde = '22-01-2005';
+        $dados_bancarios->conta = $request->nro_conta;
+        $dados_bancarios->tipo = $request->tipo_conta;
+        $dados_bancarios->conta_desde = $request->conta_desde;
         $dados_bancarios->id_cadastro = $id_cadastro;
         $dados_bancarios->save();
 
