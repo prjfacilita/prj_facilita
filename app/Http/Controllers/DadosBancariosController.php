1bfb358810c2ba017bef9b97833eb74171ca98d5
rtelesc <rtelesc@gmail.com>
2018-12-28 02:10:52 -0200

    atualização controller

--- app/Http/Controllers/DadosBancariosController.php
+++ app/Http/Controllers/DadosBancariosController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Session;
 use GuzzleHttp\Client;
-use App\DadosBancarios;
+//use App\DadosBancarios;
 
 
 class DadosBancariosController extends Controller
