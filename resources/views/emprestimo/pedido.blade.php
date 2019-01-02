f7988d0efd66af56b9787c1e2471c01cd8e463f2
rtelesc <rtelesc@gmail.com>
2019-01-02 17:18:52 -0200

    atualização formulario

--- resources/views/emprestimo/pedido.blade.php
+++ resources/views/emprestimo/pedido.blade.php
@@ -167,10 +167,13 @@
                                         <div class="solicitation-register__form">
 
                                             <!-- inicio form -->
-                                            <form action="{{route('pedido_emprestimo_parte01')}}" method="post" >
+                                            <form action="{{route('pedido_emprestimo_parte01')}}" class="pedido_emprestimo_parte01" method="post" >
                                                 {{ csrf_field() }}
                                                 <div class="col-sm-10">
-                                                    <label>Nome: <input type="text" name="solicitation-name" class="solicitation-form__name solicitation-input" onkeypress="return this.value.length<=10" ></label>
+                                                    <label>Nome: <input type="text" id="solicitation_name" name="solicitation-name" class="solicitation-form__name solicitation-input" onkeypress="return this.value.length<=30" ></label>
+                                                    <div class="valid-feedback">
+                                                        Looks good!
+                                                    </div>
                                                 </div>
 
                                                 <div class="col-sm-2">
