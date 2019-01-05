642d5e7c10eb7ba2b6d24f475fa7b7e594218e92
rtelesc <rtelesc@gmail.com>
2019-01-05 17:13:30 -0200

    correção função outro valor simulação

--- public/assets/index/js/simulacao.js
+++ public/assets/index/js/simulacao.js
@@ -1,50 +1,50 @@
-/*SCRIPT RESPONSÁVEL PELA SIMULAÇÃO DO EMPRESTIMO*/
+            /*SCRIPT RESPONSÁVEL PELA SIMULAÇÃO DO EMPRESTIMO*/
+
+
+            'use stritc';
+
+            /*variaveis publicas*/
+            var value = 0;
+            var qtdParcelas = 0;
 
 
 
-
-        /*Click para simulação*/
-
-        // let clicks = 0;
-        var value = 0;
-        var qtdParcelas = 0;
-
-        ///validar cpf
-        $('#simulation-cpf').mask('000.000.000-00', {reverse: true});
-
-        $(document).on('click','.simulation-value', function(e){
-
-            e.preventDefault();
-            e.preventDefault();
+            ///validar cpf
+            $('#simulation-cpf').mask('000.000.000-00', {reverse: true});
 
 
-            /*Verificar se houve o click*/
+            $( "#simulation-other-value" ).change(function(e) {
+                // alert( "Handler for .change() called." );
+                e.preventDefault();
 
-            if ( ! $("input[name=\"simulation-value\"]:checked").is(':checked') ){
-
-                return false;
-            }
-
-
-                if (document.querySelector('input[name="simulation-value"]:checked').value) {
-
-                    value = document.querySelector('input[name="simulation-value"]:checked').value;
-                    console.log(value);
-                }else{
+                if ( ! $("input[name=\"simulation-other-value\"]:checked").is(':checked') ){
 
                     return false;
                 }
 
 
+                if (document.querySelector('input[name="simulation-other-value"]:checked').value) {
+
+                    value = document.querySelector('input[name="simulation-other-value"]:checked').value;
+
+                }else{
+
+                    return false;
+                }
+
                 valueCorreto = document.querySelector('input[name="simulation-value"]:checked').value;
 
+
+                /*salva em váriavel global o valor*/
                 $("body").data('simulacao', valueCorreto);
+
 
                 $(".simulation-box form.pt1").css('display', 'none'); // ocultar formulario 1
                 $(".simulation-box h2.pt1").css('display', 'none'); // ocultar h2
                 $(".simulation-box form.pt2").css('display', 'block'); // ocultar formulario
                 $(".simulation-box p.pt2").css('display', 'block'); // ocultar h2
                 $(".simulation-box h2.pt2").css('display', 'block'); // ocultar h2
+
                 $(".simulation-box p.pt2").html('R$ ' + formatReal(value));
 
                 $('.banner__simulation').addClass('simulation-value-selected');
@@ -52,83 +52,112 @@
 
 
 
+            });
 
-        });
+
+
+
+            $(document).on('click','.simulation-value', function(e){
+
+                e.preventDefault();
+
+
+
+                /*Verificar se houve o click*/
+
+                    if ( ! $("input[name=\"simulation-value\"]:checked").is(':checked') ){
+
+                        return false;
+                    }
+
+
+                    if (document.querySelector('input[name="simulation-value"]:checked').value) {
+
+                        value = document.querySelector('input[name="simulation-value"]:checked').value;
+                        // console.log(value);
+                    }else{
+
+                        return false;
+                    }
+
+
+                    valueCorreto = document.querySelector('input[name="simulation-value"]:checked').value;
+
+                    $("body").data('simulacao', valueCorreto);
+
+                    $(".simulation-box form.pt1").css('display', 'none'); // ocultar formulario 1
+                    $(".simulation-box h2.pt1").css('display', 'none'); // ocultar h2
+                    $(".simulation-box form.pt2").css('display', 'block'); // ocultar formulario
+                    $(".simulation-box p.pt2").css('display', 'block'); // ocultar h2
+                    $(".simulation-box h2.pt2").css('display', 'block'); // ocultar h2
+
+                    $(".simulation-box p.pt2").html('R$ ' + formatReal(value));
+
+                    $('.banner__simulation').addClass('simulation-value-selected');
+                    $('.banner__simulation').removeClass('simulation-value');
+
+
+
+
+
+
+
+            });
 
 
 
         // console.log(value);
-        $(document).on('click','.simulation-item', function(e) {
+            $(document).on('click','.simulation-item', function(e) {
 
-            e.preventDefault();
+                e.preventDefault();
 
-            if ( ! $("input[name=\"simulation-plots\"]:checked").is(':checked') ){
+                if ( ! $("input[name=\"simulation-plots\"]:checked").is(':checked') ){
 
-                return false;
+                    return false;
+                }
+
+
+
+
+                $(".simulation-box form.pt2").css('display','none'); // ocultar formulario
+                $(".simulation-box h2.pt2").css('display','none'); // ocultar h2
+                $(".simulation-box p.pt2").css('display','none');
+
+                /*Call modal*/
+
+
+                // chamada da api
+
+                CalcularParcelas();
+                $('#exampleModal').modal('toggle');
+
+                $("form.pt3").css('display','block'); // ocultar formulario
+                $("p.pt3").css('display','block'); // ocultar h2
+                $("h2.pt3").css('display','block'); // ocultar h2
+
+
+                // var value = $("body").data('simulacao')
+                $("p.pt3").html('R$' + formatReal( $("body").data('simulacao') ));
+
+
+                qtdParcelas = document.querySelector('#pt2 input[name="simulation-plots"]:checked').value;
+
+                $("input[name='simulation-plots2'][value='"+qtdParcelas+"']").prop('checked', true);
+
+            });
+
+
+            /*FUNÇÃO PAARA FORMATAR O VALOR*/
+            function formatReal( int ) {
+                var tmp = int+'';
+                tmp = tmp.replace(/([0-9]{2})$/g, ",$1");
+                if( tmp.length > 6 )
+                    tmp = tmp.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
+                return tmp;
             }
 
 
 
-
-            $(".simulation-box form.pt2").css('display','none'); // ocultar formulario
-            $(".simulation-box h2.pt2").css('display','none'); // ocultar h2
-            $(".simulation-box p.pt2").css('display','none');
-
-            /*Call modal*/
-
-
-            // chamada da api
-
-            CalcularParcelas();
-            $('#exampleModal').modal('toggle');
-
-            $("form.pt3").css('display','block'); // ocultar formulario
-            $("p.pt3").css('display','block'); // ocultar h2
-            $("h2.pt3").css('display','block'); // ocultar h2
-
-
-            // var value = $("body").data('simulacao')
-            $("p.pt3").html('R$' + formatReal( $("body").data('simulacao') ));
-
-
-            qtdParcelas = document.querySelector('#pt2 input[name="simulation-plots"]:checked').value;
-
-            $("input[name='simulation-plots2'][value='"+qtdParcelas+"']").prop('checked', true);
-
-        });
-
-
-        /*FUNÇÃO PAARA FORMATAR O VALOR*/
-        function formatReal( int ) {
-            var tmp = int+'';
-            tmp = tmp.replace(/([0-9]{2})$/g, ",$1");
-            if( tmp.length > 6 )
-                tmp = tmp.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
-            return tmp;
-        }
-
-
-
-
-
-        /*RETORNAR MASCARA DE MOEDA PARA INPUT OUTRO VALOR INDEX SIMULAÇÃO EMPRESTIMO*/
-            // $(document).ready(function(){
-            //     $('[id^=simulation-other-value]').keypress(validateNumber);
-            //
-            //
-            // });
-            //
-            //
-            // function validateNumber(event) {
-            //     var key = window.event ? event.keyCode : event.which;
-            //     if (event.keyCode === 8 || event.keyCode === 46) {
-            //         return true;
-            //     } else if ( key < 48 || key > 57 ) {
-            //         return false;
-            //     } else {
-            //         return true;
-            //     }
-            // };
 
 
 
@@ -148,38 +177,18 @@
 
 
 
-
-// $(document).on('click', '.simular_agora', function (e) {
-
-
                 function CalcularParcelas(){
-
-
-
-
-                // <input type="text" name="simulation-name" placeholder="Nome completo" class="simulation-info"/>
-                //         <input type="text" name="simulation-cpf" id="simulation-cpf" placeholder="CPF" class="simulation-info"/>
-                //         <input type="email" name="simulation-email" placeholder="E-mail" class="simulation-info"/>
-                //         <select id="reason" class="simulation-info">
-
-                // var input_1 =
-
-
 
 
                 var valorSolicitado = $("body").data('simulacao');
                 var qteParcelas = $("input[name=simulation-plots]").val();
-                var cpf = $("input[name=simulation_cpf]").val();
-                var email = $("input[name=simulation_email]").val();
-                var name = $("input[name=simulation_name]").val();
 
-                // TestaCPF(cpf);
 
                     $.ajax({
                         type: "POST",
 
                         url:  APP_URL + '/api/simulador',
-                        data: {valorSolicitado: valorSolicitado, qteParcelas: qteParcelas, cpf:cpf, email:email, name:name},
+                        data: {valorSolicitado: valorSolicitado, qteParcelas: qteParcelas},
                     success: function( data, msg ) {
 
                         console.log(msg);
@@ -189,8 +198,7 @@
 
                         // alert(data['lastInserId']);
                         $("input[name=simulacao_id]").val(data['lastInserId']);
-                        // alert(msg);
-                        // $("#ajaxResponse").append("<div>"+msg+"</div>");
+
                     }
                     // });
                     });
@@ -200,37 +208,8 @@
 
 
 
-
-
-        function TestaCPF(strCPF) {
-            var Soma;
-            var Resto;
-            Soma = 0;
-            if (strCPF == "00000000000") return false;
-
-            for (i=1; i<=9; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
-            Resto = (Soma * 10) % 11;
-
-            if ((Resto == 10) || (Resto == 11))  Resto = 0;
-            if (Resto != parseInt(strCPF.substring(9, 10)) ) return false;
-
-            Soma = 0;
-            for (i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (12 - i);
-            Resto = (Soma * 10) % 11;
-
-            if ((Resto == 10) || (Resto == 11))  Resto = 0;
-            if (Resto != parseInt(strCPF.substring(10, 11) ) ) return false;
-            return true;
-        }
-
-
-
-
-        $('#exampleModal').on('hidden.bs.modal', function () {
-            // do something…
-            // alert('hye');
-
-            location.reload();
-        });
+                $('#exampleModal').on('hidden.bs.modal', function () {
+                    location.reload();
+                });
 
 
