abe647a4a19c4c4e6179daa9b931ae55cea8ab75
rtelesc <rtelesc@gmail.com>
2019-01-13 18:28:00 -0200

    cpf ou email já cadastrado

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -51,8 +51,15 @@
             <div class="header-content">
 
                 @if ($message = Session::get('error'))
-                    <div class="alert alert-danger">
-                        <p>{{ $message }}</p>
+                    {{--<div class="alert alert-danger">--}}
+                        {{--<p>{{ $message }}</p>--}}
+                    {{--</div>--}}
+
+                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                        <strong>Erro!</strong> {{ $message }}
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
                     </div>
                 @endif
 
