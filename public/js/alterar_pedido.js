cab0fcac8b2d0c89eb988a3a4eefbb546f60fb69
rtelesc <rtelesc@gmail.com>
2019-01-15 01:36:25 -0200

    teste

--- public/js/alterar_pedido.js
+++ public/js/alterar_pedido.js
@@ -10,6 +10,17 @@
                 // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
                 // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
                 var modal = $(this)
-                modal.find('.modal-title').text('New message to ' + recipient)
+                // modal.find('.modal-title').text('Alterar pedido')
                 modal.find('.modal-body input').val(recipient)
             })
+
+
+
+            $(document).on('click', '.salvar_alteracao_pedido', function (e) {
+                e.preventDefault();
+
+
+
+
+            })
+
--- resources/views/emprestimo/pedido.blade.php
+++ resources/views/emprestimo/pedido.blade.php
@@ -190,7 +190,7 @@
                                 </div>
                                 <div class="modal-footer">
                                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                    <button type="button" class="btn btn-primary">Salvar</button>
+                                    <button type="button" class="btn btn-success salvar_alteracao_pedido">Salvar</button>
                                 </div>
                             </div>
                         </div>
