40cd72275c45c24495e4a5feed4bb0d2105e71df
rtelesc <rtelesc@gmail.com>
2019-01-17 00:23:03 -0200

    correção email pre analise;

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -11,7 +11,7 @@ class FileController extends Controller
 
     public function store(Request $request){
 
-        $request->fileToUpload->store('logos');
+        $request->file('image')->store('logos');
 
         return $request;
     }
